2f56e497cf5e3a899e0d5fb602764aecc69db1cf
David Lindahl <davidalindahl@gmail.com>
2018-03-26 16:02:25 -0700

    added font weight bold to the title fonts

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -18,7 +18,7 @@ const Content = () => (
 		</div>
 		
 		<div className="h-64">
-			<p className="text-3xl font-title text-center container mx-auto px-2 py-2 bg-lightPrimary rounded-t-lg w-1/3">Our Mission</p>
+			<p className="text-3xl font-title text-center container mx-auto px-2 py-2 bg-lightPrimary rounded-t-lg w-1/3 font-bold">Our Mission</p>
 			<div className="flex justify-around items-center bg-lightPrimary pt-8 pb-8">
 				<div><img className=" h-48" src={analyzeAllTheThings}></img> </div>
 				<div className="w-1/2">
@@ -34,8 +34,8 @@ const Content = () => (
 		</div>
 		
 		<div className="flex items-center flex-wrap justify-center h-48 text-center w-full container mx-auto px-4">
-			<p className="text-3xl font-title text-center w-full"><span className="highlight">We do the hard work and find the best apps.</span></p>
-			<p className="text-3xl font-title text-center w-full"><span className="highlight">So you can get to enjoying life.</span></p>
+			<p className="text-3xl font-title text-center w-full font-bold"><span className="highlight">We do the hard work and find the best apps.</span></p>
+			<p className="text-3xl font-title text-center w-full font-bold"><span className="highlight">So you can get to enjoying life.</span></p>
 		</div>
 
 		<div className="flex items-center justify-center h-64 container mx-auto px-4">
@@ -44,7 +44,7 @@ const Content = () => (
 			<p className="text-lg font-body mb-2 ">Launching soon. Get notified when we go live and vote for the category you want reviewed first.</p>
 			<button className="bg-darkPrimary hover:bg-dividerColor text-white font-bold py-2 px-4 border-b-4 border-lightPrimary hover:border-secondaryText rounded">
 				<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z"/></svg>
-				<span className="font-title uppercase tracking-wide">Stay Updated</span>
+				<span className="font-title uppercase tracking-wide font-bold">Stay Updated</span>
 			</button>
 			</div>
 		</div>
