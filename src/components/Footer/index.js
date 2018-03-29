c09ca31aa5a4c6f565eca33ee018f3a343aeff07
David Lindahl <davidalindahl@gmail.com>
2018-03-29 10:36:27 -0700

    styling and content for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,14 +8,18 @@ import facebookSVG from "../../assets/icon-facebook.svg";
 
 
 const Footer = () => (
-  <div className="h-24 p-3 bg-lightPrimary ">
+  <div className="p-3 bg-lightPrimary ">
     <div className="flex justify-center container mx-auto px-4">
       <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={twitterSVG}></img></a>
       <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={mediumSVG}></img></a>
       <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={facebookSVG}></img></a>
     </div>
-    <p className="text-primaryText text-xs w-full text-center"> Are you a passionate android fan with a background in writing? Email us a brief bio and include previous writing samples. We are always looking for the right contributors to join our growing team. </p>
-    <p className="text-primaryText text-xs w-full text-center"> © 2018 | All rights reserved.</p>
+    <div className="flex-col justify-center py-4">
+      <p className="text-primaryText text-xs leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
+      <p className="text-primaryText text-xs leading-normal text-center">Email us a brief bio and include previous writing samples.</p>
+      <p className="text-primaryText text-xs leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
+    </div>
+    <p className="text-primaryText text-xs w-full leading-normal text-center"> © 2018 | All rights reserved.</p>
   </div>
 )
 
