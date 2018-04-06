192fbda1126480692ff54914d19b7fa24b38a005
David Lindahl <davidalindahl@gmail.com>
2018-04-06 09:11:25 -0700

    added in text background shadow to the footer, updated footer background color and changed svg in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,17 +8,17 @@ import facebookSVG from "../../assets/icon-facebook.svg";
 
 
 const Footer = () => (
-  <div className="p-3 bg-lightPrimary antialiased">
+  <div className="p-3 bg-darkPrimary antialiased">
     <div className="flex justify-center container mx-auto px-4">
-      <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={twitterSVG}></img></a>
-      <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={mediumSVG}></img></a>
-      <a href="#"><img className="h-4 fill-current text-darkPrimary rounded m-2" src={facebookSVG}></img></a>
+      <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={twitterSVG}></img></a>
+      <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={mediumSVG}></img></a>
+      <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={facebookSVG}></img></a>
     </div>
-    <p className="text-primaryText text-xs w-full leading-normal text-center"> © 2018 | All rights reserved | <a href="#" className="text-primaryText text-xs w-full leading-normal text-center">Contact Us</a></p>
+    <p className="text-textIcons text-xs w-full leading-normal text-center"> © 2018 | All rights reserved | <a href="#" className="text-textIcons text-xs w-full leading-normal text-center">Contact Us</a></p>
     <div className="flex-col justify-center py-4">
-      <p className="text-primaryText text-xs leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
-      <p className="text-primaryText text-xs leading-normal text-center">Shoot us an <a href="#" className="text-primaryText">email</a> some words about yourself and links to things you've written.</p>
-      <p className="text-primaryText text-xs leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
+      <p className="text-textIcons text-xs leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
+      <p className="text-textIcons text-xs leading-normal text-center">Shoot us an <a href="#" className="text-textIcons">email</a> some words about yourself and links to things you've written.</p>
+      <p className="text-textIcons text-xs leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
     </div>
     
   </div>
