4062a4bad4ab3db7793a13de12939a0441bae59e
David Lindahl <davidalindahl@gmail.com>
2018-04-06 09:12:32 -0700

    added font body to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,11 +14,11 @@ const Footer = () => (
       <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={mediumSVG}></img></a>
       <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={facebookSVG}></img></a>
     </div>
-    <p className="text-textIcons text-xs w-full leading-normal text-center"> © 2018 | All rights reserved | <a href="#" className="text-textIcons text-xs w-full leading-normal text-center">Contact Us</a></p>
+    <p className="font-body text-textIcons text-xs w-full leading-normal text-center"> © 2018 | All rights reserved | <a href="#" className="text-textIcons text-xs w-full leading-normal text-center">Contact Us</a></p>
     <div className="flex-col justify-center py-4">
-      <p className="text-textIcons text-xs leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
-      <p className="text-textIcons text-xs leading-normal text-center">Shoot us an <a href="#" className="text-textIcons">email</a> some words about yourself and links to things you've written.</p>
-      <p className="text-textIcons text-xs leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
+      <p className="font-body text-textIcons text-xs leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
+      <p className="font-body text-textIcons text-xs leading-normal text-center">Shoot us an <a href="#" className="text-textIcons">email</a> some words about yourself and links to things you've written.</p>
+      <p className="font-body text-textIcons text-xs leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
     </div>
     
   </div>
