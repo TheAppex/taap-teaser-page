839e701a15b63cffb0d2bf7789feeb3038d3cbdb
David Lindahl <davidalindahl@gmail.com>
2018-04-09 16:43:30 -0700

    updates to the our mission section with animations

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -56,13 +56,13 @@ const Content = () => (
 		</div>
 		
 		<div className="">
-			<p className="text-primaryText uppercase tracking-wide text-2xl  md:text-3xl lg:text-3xl xl:text-3xl font-title text-center container mx-auto px-4 py-2 pt-4 bg-lightPrimary rounded-t-lg w-2/3 sm:w-2/3 md:w-1/3 lg:w-1/3 xl:w-1/3 font-bold">Our Mission</p>
+			<p className="text-primaryText uppercase tracking-wide text-2xl  md:text-3xl lg:text-3xl xl:text-3xl font-title text-center container mx-auto px-4 py-2 pt-4 bg-lightPrimary rounded-t-lg w-2/3 sm:w-2/3 md:w-1/3 lg:w-1/3 xl:w-1/3 font-bold" data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">Our Mission</p>
 			
-		<div className="flex-col sm:flex sm:flex-row content-around items-center bg-lightPrimary shadow pt-8 pb-8" data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">
-			<div className="sm:w-1/2">
+		<div className="flex-col sm:flex sm:flex-row content-around items-center bg-lightPrimary shadow pt-8 pb-8">
+			<div className="sm:w-1/2" data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">
 				<img className="px-8 py-4 max-h-360" src={analyzeAllTheThings}></img> 
 			</div>
-			<div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+			<div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 " data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">
 				<ul className="list-reset mission-list px-8">
 					<li className="text-primaryText text-sm md:text-base lg:text-base xl:text-base font-body pb-4 leading-normal">Provide the most comprehensive 
 					reviews for the best Android Apps, 
