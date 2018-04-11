eb74bde14068f8a384ac37f369d4abf9028fbdb6
David Lindahl <davidalindahl@gmail.com>
2018-04-11 12:50:52 -0700

    added letter spacing to all caps vote

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -34,7 +34,7 @@ const Content = () => (
 
 			
 				<button className="p-2 bg-darkPrimary hover:bg-primary items-center text-textIcons leading-none rounded-full flex lg:inline-flex mt-2" role="alert">
-					<span className="flex rounded-full bg-lightPrimary uppercase px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary mr-3">Vote</span>
+					<span className="flex rounded-full bg-lightPrimary uppercase tracking-wide px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary mr-3">Vote</span>
 					<span className="font-body font-semibold mr-2 text-base xl:text-lg text-left flex-auto">Tell us which category you want reviewed</span>
 					<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
 				</button>
