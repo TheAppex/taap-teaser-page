d4fbb6fc21603e00e4eb13cdd262c6c537cd7cea
David Lindahl <davidalindahl@gmail.com>
2018-04-12 16:02:41 -0700

    tried to do a gradient thing and it didnt work out

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -59,7 +59,7 @@ const Content = () => (
 	<div className="">
 		<p className="text-primaryText uppercase tracking-wide text-2xl  md:text-3xl lg:text-3xl xl:text-3xl font-title text-center container mx-auto px-4 py-2 pt-4 bg-lightPrimary rounded-t-lg w-2/3 sm:w-2/3 md:w-1/3 lg:w-1/3 xl:w-1/4 font-bold" data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">Our Mission</p>
 			
-		<div className="flex-col sm:flex sm:flex-row content-around items-center bg-lightPrimary shadow pt-8 pb-8">
+		<div className="flex-col sm:flex sm:flex-row content-around items-center bg-gradient shadow pt-8 pb-8">
 			<div className="text-center w-full md:w-1/2 lg:w-1/2 xl:w-1/2 " data-aos="fade" data-aos-duration="1200" data-aos-delay="300" data-aos-once="true">
 				<img className="px-8 py-4 max-h-360" src={analyzeAllTheThings}></img> 
 			</div>
