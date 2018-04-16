cf0af90af23b1c0507cb0fa315f43051f5f34441
David Lindahl <davidalindahl@gmail.com>
2018-04-16 14:54:18 -0700

    added shadows the stay updated buttons

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -22,9 +22,9 @@ const Content = () => (
 			<form name="newsletter" action="/success" method="post" data-netlify="true" data-netlify-honeypot="bot-field" className="w-full max-w-sm">
     		<input type="hidden" name="bot-field" />
 				<div className="flex items-center py-2">
-					<input className="appearance-none bg-transparent border-none w-full text-primaryText mr-3 py-2 px-2 bg-grey-lighter text-base xl:text-lg rounded" type="text" placeholder="Enter your email" aria-label="Full name"></input>
+					<input className="appearance-none bg-transparent border-none w-full text-primaryText mr-3 py-2 px-2 bg-grey-lighter text-base xl:text-lg rounded  shadow-2" type="text" placeholder="Enter your email" aria-label="Full name"></input>
 					
-					<button className="flex-no-shrink bg-darkPrimary hover:bg-primary text-lightPrimary font-bold py-2 px-4 rounded" type="button">
+					<button className="flex-no-shrink bg-darkPrimary hover:bg-primary text-lightPrimary font-bold py-2 px-4 rounded btn-hover shadow-2" type="button">
 						<span className="text-base xl:text-xl font-bold text-lightPrimary uppercase tracking-wide">Stay Updated</span>
 					</button>
 					
@@ -33,9 +33,9 @@ const Content = () => (
 			
 
 			
-				<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2" role="alert">
+				<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert">
 					<span className="flex rounded-full bg-textIcons px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary uppercase tracking-wide mr-3">Vote</span>
-					<span className="font-body font-semibold mr-2 text-base xl:text-lg text-left flex-auto">Tell us which category you want reviewed</span>
+					<span className="font-body font-semibold mr-2 text-sm xl:text-base text-left flex-auto">Tell us which category you want reviewed</span>
 					<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
 				</button>
 			
--- tailwind.config.js
+++ tailwind.config.js
@@ -755,6 +755,7 @@ module.exports = {
     'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
     'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
     'none': 'none',
+    '2': '0 4px 8px rgba(0,0,0,0.1)',
   },
 
 
