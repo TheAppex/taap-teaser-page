6453ef0e348c96fd370703930333cac75390a2d7
David Lindahl <davidalindahl@gmail.com>
2018-05-07 17:13:02 -0700

    updated UI for success and not success

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -16,7 +16,8 @@ class Content extends React.Component {
 	constructor() {
     super()
     this.state = {
-      email: ``,
+			email: ``,
+			status: `success`,
 		}
 	this._handleEmailChange = this._handleEmailChange.bind(this);
 	this._handleFormSubmit = this._handleFormSubmit.bind(this);
@@ -95,6 +96,15 @@ class Content extends React.Component {
 										<span className="text-base xl:text-xl font-bold text-textIcons uppercase tracking-wide">put check box here</span>
 									</button>
 									
+									
+								</div>
+
+								<div>
+									<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert">
+										<span className="flex rounded-full bg-textIcons px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary uppercase tracking-wide mr-3">Vote</span>
+										<span className="font-body font-semibold mr-2 text-sm xl:text-base text-left flex-auto">Tell us which category you want reviewed</span>
+										<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+									</button>
 								</div>
 							</div>
 						</div>
@@ -138,12 +148,12 @@ class Content extends React.Component {
 								</div>
 							</form>
 							<div>
-						<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert">
-							<span className="flex rounded-full bg-textIcons px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary uppercase tracking-wide mr-3">Vote</span>
-							<span className="font-body font-semibold mr-2 text-sm xl:text-base text-left flex-auto">Tell us which category you want reviewed</span>
-							<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-						</button>
-					</div>
+								<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert">
+									<span className="flex rounded-full bg-textIcons px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary uppercase tracking-wide mr-3">Vote</span>
+									<span className="font-body font-semibold mr-2 text-sm xl:text-base text-left flex-auto">Tell us which category you want reviewed</span>
+									<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+								</button>
+							</div>
 						</div>
 					)}
 					
