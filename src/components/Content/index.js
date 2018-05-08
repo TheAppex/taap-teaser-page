0b016b07c010126b8a63b8c80c42306e69c96216
David Lindahl <davidalindahl@gmail.com>
2018-05-08 15:31:45 -0700

    added link to the trello board for voting

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -146,11 +146,11 @@ class Content extends React.Component {
 								</div>
 							</form>
 							<div>
-								<button className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert">
+								<a className="p-2 bg-lightPrimary hover:bg-primary items-center text-darkPrimary hover:text-textIcons leading-none rounded-full flex lg:inline-flex mt-2 shadow-2" role="alert" href="https://trello.com/b/n6mTFQ9J" target="_blank">
 									<span className="flex rounded-full bg-textIcons px-2 py-1 text-xs xl:text-base font-bold text-darkPrimary uppercase tracking-wide mr-3">Vote</span>
 									<span className="font-body font-semibold mr-2 text-sm xl:text-base text-left flex-auto">Tell us which category you want reviewed</span>
 									<svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-								</button>
+								</a>
 							</div>
 						</div>
 					)}
