6accea02e4681c18585325538ebc4f226eee929c
David Lindahl <davidalindahl@gmail.com>
2018-05-23 14:20:35 -0700

    updated footer email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,10 +14,10 @@ const Footer = () => (
       <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={mediumSVG}></img></a>
       <a href="#"><img className="h-4 fill-current text-textIcons rounded m-2" src={facebookSVG}></img></a>
     </div>
-    <p className="font-body text-textIcons text-xs xl:text-sm w-full leading-normal text-center"> © 2018 · All rights reserved · <a href="mailto:hello@theappex.com" className="text-textIcons text-xs xl:text-sm w-full leading-normal text-center">Contact Us</a></p>
+    <p className="font-body text-textIcons text-xs xl:text-sm w-full leading-normal text-center"> © 2018 · All rights reserved · <a href="mailto:info@theappex.com" className="text-textIcons text-xs xl:text-sm w-full leading-normal text-center">Contact Us</a></p>
     <div className="flex-col justify-center py-4">
       <p className="font-body text-textIcons text-xs xl:text-sm leading-normal text-center">P.S. Are you a passionate android fan with a background in writing?</p>
-      <p className="font-body text-textIcons text-xs xl:text-sm leading-normal text-center">Shoot us an <a href="mailto:hello@theappex.com" className="text-textIcons">email</a> with some words about yourself and links to things you've written.</p>
+      <p className="font-body text-textIcons text-xs xl:text-sm leading-normal text-center">Shoot us an <a href="mailto:info@theappex.com" className="text-textIcons">email</a> with some words about yourself and links to things you've written.</p>
       <p className="font-body text-textIcons text-xs xl:text-sm leading-normal text-center">We are always looking for the right contributors to join our growing team.</p>
     </div>
     
